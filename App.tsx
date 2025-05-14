694d6f0f1cbbe597aa1a836245977fb776bdcbf5
eedwardpark <32019573+eedwardpark@users.noreply.github.com>
2025-05-14 01:25:26 -0700

    style formatting

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WorkoutScreen from "./screens/WorkoutScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -22,6 +23,10 @@ export default function App() {
         <Stack.Screen
           name="Home"
           component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Workout"
+          component={WorkoutScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
